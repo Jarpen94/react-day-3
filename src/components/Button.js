0ac8d3a83ead5ac8a5930344f42e043e1ff92503
Jarpen94 <kwiatowskijarek@o2.pl>
2018-11-21 10:29:21 +0100

    Adding Button

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import RaisedButton from 'material-ui/RaisedButton'
+
+
+const style = {
+    margin: 50,
+};
+
+
+const Button = (props) => (
+    <div>
+        <RaisedButton
+            label="Primary"
+            primary={true}
+            style={style}
+            onClick={props.onClick}
+        />
+    </div>
+)
+
+export default Button 
